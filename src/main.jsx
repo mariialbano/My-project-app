5faeba96f029bfc9cb12d5fba6dcb39d9481d38d
mariialbano <marianatalbano@gmail.com>
2023-09-05 16:09:44 -0300

    importando o Outlet e corrigindo erros no main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import './index.css'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import Home from './routes/Home/index.jsx'
 import Produtos from './routes/Produtos/index.jsx'
-import Error from './routes/EditarPodutos/index.jsx'
+import Error from './routes/Error/index.jsx'
 import EditarProduto from './routes/EditarPodutos/index.jsx'
 
 const router = createBrowserRouter([
