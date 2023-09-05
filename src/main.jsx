c280e10af9da3917623e6c42e36dd87685f4bcdf
mariialbano <marianatalbano@gmail.com>
2023-09-05 15:42:33 -0300

    páginas importadas no main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Home from './routes/Home/index.jsx'
+import Produtos from './routes/Produtos/index.jsx'
+import Error from './routes/EditarPodutos/index.jsx'
+import EditarProduto from './routes/EditarPodutos/index.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
 )
+
