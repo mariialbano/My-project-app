58c4d3335a9ccea3d6c531830321276ced9a6dad
mariialbano <marianatalbano@gmail.com>
2023-10-06 09:05:11 -0300

    componentes inseridos

--- src/components/rodape.jsx
+++ src/components/rodape.jsx
@@ -1,8 +1,22 @@
-export default function Rodape(){
-    
-    return(
+export default function Rodape() {
+    return (
+      <>
         <footer>
-            <p>Rodapé</p>
+          <div>
+            <ul>
+              <li>
+                <a href="#">GitHub</a>
+              </li>
+              <li>
+                <a href="#">X</a>
+              </li>
+              <li>
+                <a href="#">Reddit</a>
+              </li>
+            </ul>
+            <p>&copy; Todos os direitos reservados - 2023</p>
+          </div>
         </footer>
-    )
-}
+      </>
+    );
+  }
