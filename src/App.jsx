846b039f8b0c96ad76962f30fcafc5d24b01cb83
mariialbano <marianatalbano@gmail.com>
2023-10-20 08:23:17 -0300

    App.module.css inserido

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Menu from './components/menu'
-import Rodape from './components/rodape'
-import { Outlet } from 'react-router-dom'
+//Importando dependências e ou arquivos que vamos utilizar e ou referenciar.
+import { Outlet } from "react-router-dom";
+import Cabecalho from "./components/Cabecalho/Cabecalho";
+import Rodape from "./components/Rodape";
+import styles from "./App.module.css";
+// import { useState } from "react";
 
-function App() {
-  
+export default function App() {
+
+  // const [produto, setProduto] = useState("PRODUTO");
+
+  // let count = 0;
+  // const novoValor =()=>{
+  //   count++;
+  //   setProduto(produto +" "+ count);
+  // }
 
   return (
-    <>
-      <Menu/>
-      <Outlet/>
-      <Rodape/>
-    </>
-  )
-}
+    
+      <div className={styles.container}>
+        {/* Isso é um comentário */}
 
-export default App
+        {/* Vamos criar uma cabeçalho com um header, um h1 com um título e uma lista ul com 3 item com link. */}
+        <Cabecalho />
+        {/* Vamos criar uma seção com uma div, com 3 <p></p> e estes <p></p> vão ter 3 linhas de lorem, abaixo dos <p></p> uma img. */}
+            {/* <p>Valor do useState :{produto}</p>
+            <button onClick={novoValor}>CLICK</button> */}
+            <Outlet/>
+
+        <Rodape />
+        {/* Vamos criar um rodapé com uma div, uma lista com 3 itens com links para redes sociais e abaixo um <p></p> com o texto e código do símbolo de direitos reservados. */}
+      </div>
+    );
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,42 +1,29 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
-import { createBrowserRouter, RouterProvider, Navigate } from 'react-router-dom'
-import Home from './routes/Home/index.jsx'
-import Produtos from './routes/Produtos/index.jsx'
-import Error from './routes/Error/index.jsx'
-import EditarProduto from './routes/EditarPodutos/index.jsx'
+
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Home from './routes/Home.jsx';
+import EditarProdutos from './routes/EditarProdutos.jsx';
+import Erro404 from './routes/Erro404.jsx';
+import Produtos from './routes/Produtos.jsx';
+import ExcluirProduto from './routes/ExcluirProduto.jsx';
+import InserirProduto from './routes/InserirProduto.jsx';
 
 const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App/>,
-    errorElement: <Error/>,
+  {path: '/', element: <App/>,
+    errorElement: <Erro404/>,
     children:[
-      {
-        path: "/",
-        element: <Home/>
-      },
-      {
-        path: "/produtos",
-        element: <Produtos/>
-      },
-      {
-        path: "/produtos/editar/:id",
-        element: <EditarProduto/>
-      },
-      {
-        path: "/antiga",
-        element: <Navigate to="/" />
-      }
+      {path: '/', element: <Home/>},
+      {path: '/produtos', element: <Produtos/>},
+      {path: '/editar/produtos/:id', element: <EditarProdutos/>},
+      {path: '/excluir/produtos/:id', element: <ExcluirProduto/>},
+      {path: '/cadastrar/produto', element: <InserirProduto/>},
     ]
   }
-])
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>,
+  
+       <RouterProvider router={router}/>
+  ,
 )
-
